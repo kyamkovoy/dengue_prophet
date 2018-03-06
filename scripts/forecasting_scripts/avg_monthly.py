b6062ffdec89ca90a98651ad4ed734dc7ae6ee6f
Kristina <kyamkovoy@umass.edu>
2018-03-06 11:50:25 -0500

    worked on hist_avg script (still not complete)

--- scripts/forecasting_scripts/avg_monthly.py
+++ scripts/forecasting_scripts/avg_monthly.py
@@ -12,11 +12,39 @@ with open('../../output/cv_df_list_prospective_monthly.pkl', 'rb') as file:
     data_file = pickle.load(file)
 
 
-for j in range(1, 11):  # want to predict at 10 time intervals, from one year forward to 10 years forward
-    how_many_years_forward = j
-    how_many_months = 12 * how_many_years_forward
+def get_monthly_avg(df):
+    # gets average values for each month in the df
+    df['year_month'] = (df['date_sick_year']-2006)*12 + (df['month'])
 
-    for prov in province_codes:
+    # want to first smooth the data
+    x = np.array(df['year_month'])
+    y = np.array(df['cases'])
+    y_sum = np.cumsum(y)
+    spl = UnivariateSpline(x, y_sum)
+    y_fit = spl(x)
+    y_smooth = np.diff(y_fit)
+    n = np.append(y_smooth[::-1], y_sum[0])
+    y_smooth = n[::-1]
+    df['smooth_cases'] = y_smooth
+
+    # then get the averages
+    monthly_avg = []
+    for i in range(1, 13):
+        monthly_cases = np.array(df['smooth_cases'].loc[df['month'] == i])
+        avg = np.mean(monthly_cases)
+        monthly_avg.append(avg)
+
+    return monthly_avg
+
+
+for prov in province_codes:
+
+    past_predictions = []
+
+    for j in range(1, 11):  # want to predict at 10 time intervals, from one year forward to 10 years forward
+        how_many_years_forward = j
+        how_many_months = 12 * how_many_years_forward
+
         for k in range(0, 11 - how_many_years_forward):
             print()
             print('forecasting ' + str(j) + ' year(s) forward for province ' + str(prov) + ' for year ' + str(2006 + k + j))
@@ -24,11 +52,8 @@ for j in range(1, 11):  # want to predict at 10 time intervals, from one year fo
 
             df = data_file[k] # predicting for year 2007 + index
 
-            monthly_avg = []
-            for i in range(1, 13):
-                monthly_cases = np.array(df['cases'].loc[df['month'] == i])
-                avg = np.mean(monthly_cases)
-                monthly_avg.append(avg)
+            # get the monthly avg values
+            monthly_avg = get_monthly_avg(df)
 
             year_total = sum(monthly_avg)
             year_peak = max(monthly_avg)
@@ -38,7 +63,7 @@ for j in range(1, 11):  # want to predict at 10 time intervals, from one year fo
 
 
             # save everything
-            all_folder_name = "../../output/hist_avg/all_prov_monthly/all_prov_monthly_" + str(how_many_years_forward)
+            all_folder_name = "../../output/hist_avg_test/all_prov_monthly/all_prov_monthly_" + str(how_many_years_forward)
 
             if not os.path.exists(all_folder_name):
                 os.makedirs(all_folder_name)
