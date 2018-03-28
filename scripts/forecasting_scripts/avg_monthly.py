bd9af2dfd0ab2fdd41eb46fad70dc9a9f2e4d316
kyamkovoy <kyamkovoy@umass.edu>
2018-03-28 16:21:56 -0400

    got rid of biweek stuff

--- scripts/forecasting_scripts/avg_monthly.py
+++ scripts/forecasting_scripts/avg_monthly.py
@@ -39,44 +39,38 @@ def get_monthly_avg(df):
 
 for prov in province_codes:
 
-    # past_predictions = []
+    for k in range(0, 10):
+        print()
+        print('forecasting for province ' + str(prov) + ' for year ' + str(2007 + k))
+        print()
 
-    for j in range(1, 11):  # want to predict at 10 time intervals, from one year forward to 10 years forward
-        how_many_years_forward = j
-        how_many_months = 12 * how_many_years_forward
+        df = data_file[k] # predicting for year 2007 + index
+        df = df.loc[df['province'] == prov]
 
-        for k in range(0, 11 - how_many_years_forward):
-            print()
-            print('forecasting ' + str(j) + ' year(s) forward for province ' + str(prov) + ' for year ' + str(2006 + k + j))
-            print()
+        # get the monthly avg values
+        monthly_avg = get_monthly_avg(df)
 
-            df = data_file[k] # predicting for year 2007 + index
-            df = df.loc[df['province'] == prov]
+        year_total = sum(monthly_avg)
+        year_peak = max(monthly_avg)
+        peak_month = monthly_avg.index(year_peak) + 1
 
-            # get the monthly avg values
-            monthly_avg = get_monthly_avg(df)
-
-            year_total = sum(monthly_avg)
-            year_peak = max(monthly_avg)
-            peak_month = monthly_avg.index(year_peak) + 1
-
-            all_targets = {'year_total': year_total, 'year_peak': year_peak, 'peak_month': peak_month, 'month_cases': monthly_avg}
+        all_targets = {'year_total': year_total, 'year_peak': year_peak, 'peak_month': peak_month, 'month_cases': monthly_avg}
 
 
-            # save everything
-            all_folder_name = "../../output/hist_avg_test/all_prov_monthly/all_prov_monthly_" + str(how_many_years_forward)
+        # save everything
+        all_folder_name = "../../output/hist_avg/all_prov_monthly/all_prov_monthly"
 
-            if not os.path.exists(all_folder_name):
-                os.makedirs(all_folder_name)
+        if not os.path.exists(all_folder_name):
+            os.makedirs(all_folder_name)
 
-            folder_name = all_folder_name + "/" + "prov_" + str(prov) + "_monthly_" + str(how_many_years_forward)
-            file_name = "prov_" + str(prov) + "_for_" + str(2006 + k + j) + "_monthly_" + str(how_many_years_forward) + ".pkl"
+        folder_name = all_folder_name + "/" + "prov_" + str(prov) + "_monthly"
+        file_name = "prov_" + str(prov) + "_for_" + str(2007 + k) + "_monthly.pkl"
 
-            if not os.path.exists(folder_name):
-                os.makedirs(folder_name)
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
 
-            print()
-            print("saving output to " + file_name)  # CHANGE PROV AND YEAR HERE
+        print()
+        print("saving output to " + file_name)  # CHANGE PROV AND YEAR HERE
 
-            with open(folder_name + '/' + file_name, 'wb') as file:  # CHANGE PROV AND YEAR HERE
-                pickle.dump(all_targets, file)
+        with open(folder_name + '/' + file_name, 'wb') as file:  # CHANGE PROV AND YEAR HERE
+            pickle.dump(all_targets, file)
