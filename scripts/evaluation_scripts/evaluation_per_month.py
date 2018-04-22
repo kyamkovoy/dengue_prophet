d4125ab8b63689a20ac2247453cecdb672875e33
kyamkovoy <kyamkovoy@umass.edu>
2018-04-22 16:32:49 -0400

    finished eval script (i hope)

--- scripts/evaluation_scripts/evaluation_per_month.py
+++ scripts/evaluation_scripts/evaluation_per_month.py
@@ -15,54 +15,40 @@ folder_name = '../../output/monthly_forecasts/' + which_model + '/'
 
 # -----------------------------------------------------------------------------------------------------------------
 
-cv_df = pd.DataFrame(columns=['province', 'year', 'month', 'error'])
-
 for prov in province_codes:
     prov_folder = folder_name + 'prov_' + str(prov) + '_monthly/'
+    prov_data = data.loc[data['province'] == prov]
 
     for year in range(2008, 2017):
         for month in range(1, 13):
+            # load the file for forecasts starting in month, year and get forecasts into an array
             file_name = prov_folder + 'prov_' + str(prov) + '_' + str(year) + '_' + str(month) + '_monthly.csv'
             forecast_df = pd.read_csv(file_name)
-
             forecast_cases = np.array(forecast_df['yhat'])
 
+            # list of months in order, ie if month = 3, list starts at 3, ends at 2, contains 12 months
+            months_first = np.arange(month, 13)
+            months_last = np.arange(1, month)
+            month_list = np.concatenate([months_first, months_last])
 
+            # do the same thing with years
+            year_list = [year if i >=month else year + 1 for i in month_list]
 
+            # now get the list of true cases
+            true_cases = []
+            for i in range(0, 12):
+                year = year_list[i]
+                month = month_list[i]
+                cases_df = float(prov_data['cases'].loc[(prov_data['date_sick_year'] == year) &  (prov_data['month'] == month)])
+                true_cases.append(cases_df)
 
+            true_cases = np.array(true_cases)
 
+            # get the errors and put everything into a dataframe
+            errors = abs(true_cases - forecast_cases)
 
+            error_dict = {'year': year_list, 'month': month_list, 'error': errors}
+            error_df = pd.DataFrame(error_dict)
 
-    for k in range(0, 10):
-
-        year = 2007 + k
-
-        print()
-        print('province ' + str(prov) + ': year ' + str(year))
-
-        file_name = '/prov_' + str(prov) + '_monthly/prov_' + str(prov) + '_for_' + str(year) + '_monthly.pkl'
-
-        with open(folder_name + file_name, 'rb') as file:
-            forecast = pickle.load(file)
-
-        true_df = data.loc[(data['province'] == prov) & (data['date_sick_year'] == year)]
-
-        month_cases = true_df['cases'].tolist()
-        total = sum(month_cases)
-        peak = max(month_cases)
-        peak_month = month_cases.index(peak) + 1
-
-        if len(month_cases) == len(forecast['month_cases']):
-            case_rmse = np.sqrt(mse(month_cases, forecast['month_cases']))
-        else:
-            case_rmse = 'NaN'
-        year_total_error = abs(total - forecast['year_total'])
-        peak_error = abs(peak - forecast['year_peak'])
-        peak_month_error = abs(peak_month - forecast['peak_month'])
-
-        row = [prov, year, case_rmse, year_total_error, peak_error, peak_month_error]
-
-        cv_df = cv_df.append(pd.Series(row, index=cv_df.columns), ignore_index = True)
-
-
-cv_df.to_csv(folder_name + '/' + 'monthly_errors.csv', index=False)
+            save_file = prov_folder + 'prov_' + str(prov) + '_' + str(year) + '_' + str(month) + '_monthly_errors.csv'
+            error_df.to_csv(save_file, index=False)
